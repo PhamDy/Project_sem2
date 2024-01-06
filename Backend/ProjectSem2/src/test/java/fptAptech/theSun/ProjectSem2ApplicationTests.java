a938dd31d33a86b7b10bdf73467f1855fab33541
phamdacdy <phamdacdy@gmail.com>
2024-01-06 13:52:34 +0700

    1

---
+++ Backend/ProjectSem2/src/test/java/fptAptech/theSun/ProjectSem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fptAptech.theSun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSem2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
