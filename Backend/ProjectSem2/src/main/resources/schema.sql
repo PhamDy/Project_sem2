54a4d062f50239e6ae9d6be99887accb8a439943
phamdacdy <phamdacdy@gmail.com>
2024-01-09 18:14:02 +0700

    Entity

--- Backend/ProjectSem2/src/main/resources/schema.sql
+++ Backend/ProjectSem2/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `category` (
    `category_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
-     `name` VARCHAR(100) NOT NULL,
-    `status` VARCHAR(50) NOT NULL,
+     `name` VARCHAR(100) NOT NULL UNIQUE,
+    `status` ENUM('Visible', 'Invisible'),
     `updated_at` TIMESTAMP NOT NULL,
     `created_by` VARCHAR(50) NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -10,21 +10,21 @@ CREATE TABLE IF NOT EXISTS `category` (
 
 CREATE TABLE IF NOT EXISTS `product` (
     `product_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
-     `category_id` BIGINT NOT NULL,
-      `product_name` VARCHAR(100) NOT NULL,
+    `category_id` BIGINT NOT NULL,
+    `product_name` VARCHAR(100) NOT NULL,
     `avartar_image_product` VARCHAR(100) NOT NULL,
     `image1_product` VARCHAR(100) NULL,
     `image2_product` VARCHAR(100) NULL,
     `image3_product` VARCHAR(100) NULL,
     `quantity` INT NOT NULL,
     `description` TEXT NOT NULL,
-    `gender` VARCHAR(50) NOT NULL,
+    `gender` ENUM('Men', 'Women', 'Unisex'),
     `size` VARCHAR(50) NOT NULL,
     `brand` VARCHAR(50) NOT NULL,
     `color` VARCHAR(50) NOT NULL,
     `price` DOUBLE NOT NULL,
     `sport` VARCHAR(50) NOT NULL,
-    `status` VARCHAR(50) NOT NULL,
+    `status` ENUM('InStock', 'OutOfStock'),
     `discount` FLOAT NULL,
     FOREIGN KEY (`category_id`) REFERENCES category(category_id),
     `updated_at` TIMESTAMP NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 
 CREATE TABLE IF NOT EXISTS `role` (
     `role_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL,
+    `name` ENUM('ROLE_ADMIN', 'ROLE_EMPLOYEE', 'ROLE_ANONYMOUS'),
     `updated_at` TIMESTAMP NOT NULL,
     `created_by` VARCHAR(50) NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `role` (
 
 CREATE TABLE IF NOT EXISTS `user` (
      `user_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
-     `username` VARCHAR(100) NOT NULL,
+     `username` VARCHAR(100) NOT NULL UNIQUE,
     `password` VARCHAR(100) NOT NULL,
     `avatar` VARCHAR(100) NOT NULL,
     `updated_at` TIMESTAMP NOT NULL,
@@ -94,8 +94,8 @@ CREATE TABLE IF NOT EXISTS `address` (
 
 CREATE TABLE IF NOT EXISTS `payment` (
     `payment_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
-    `payment_method` VARCHAR(100) NOT NULL,
-    `status` VARCHAR(100) NOT NULL,
+    `payment_method` ENUM('ONLINE', 'CODING'),
+    `status` ENUM('Success', 'Pending', 'Delivering', 'Cancel'),
     `updated_at` TIMESTAMP NOT NULL,
     `created_by` VARCHAR(50) NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -105,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `payment` (
 CREATE TABLE IF NOT EXISTS `delivery` (
     `delivery_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
     `address_id` BIGINT NOT NULL,
-    `delivery_status` VARCHAR(100) NOT NULL,
+    `delivery_status` ENUM('Success', 'Delivering', 'Cancel'),
     FOREIGN KEY (`address_id`) REFERENCES address(address_id),
     `updated_at` TIMESTAMP NOT NULL,
     `created_by` VARCHAR(50) NOT NULL,
