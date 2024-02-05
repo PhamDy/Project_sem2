753ea660e1c62c26f91350bfc1502246fa181018
Pham Dac Dy <phamdacdy@gmail.com>
2024-02-05 23:41:18 +0700

    1

---
+++ Backend/ProjectSem2/target/classes/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO `category` (`name`,`status`, `created_by`)
+VALUES ('clothes', 'Visible','Admin');
+
+INSERT INTO `category` (`name`,`status`, `created_by`)
+VALUES ('shoes', 'Visible','Admin');
+
+INSERT INTO `category` (`name`,`status`, `created_by`)
+VALUES ('pants', 'Visible','Admin');
+
+UPDATE `category` SET  NAME = 'a' WHERE category_id = 1;
+UPDATE `category` SET  NAME = 'b' WHERE category_id = 2;
+
+
+INSERT INTO `product` (`product_name`, category_id, `avartar_image_product`, `image1_product`,`image2_product`,
+                       `image3_product`,`description`,`gender`,`brand`,
+                       `price`,`sport`,`status`,`discount`,`created_by`)
+VALUES ('Adidas Ultra Boost 5.0', 2 ,'https://img.upanh.tv/2024/01/07/adidas1.1.jpg', 'https://img.upanh.tv/2024/01/07/adidas1.2.jpg',
+        'https://img.upanh.tv/2024/01/07/adidas1.3.jpg', 'https://img.upanh.tv/2024/01/07/adidas1.4.jpg','High-performance running shoes with Boost technology for energy return.',
+        'Men','Adidas', 40, 'running', 'InStock', 9.5, 'admin' );
+
+INSERT INTO `product` (`product_name`, category_id, `avartar_image_product`, `image1_product`,`image2_product`,
+                       `image3_product`,`description`,`gender`, `brand`,
+                       `price`,`sport`,`status`,`discount`,`created_by`)
+VALUES ('Adidas NMD R1 Primeknit', 2 ,'https://img.upanh.tv/2024/01/07/adidas2.1.jpg', 'https://img.upanh.tv/2024/01/07/adidas2.2.jpg',
+        'https://img.upanh.tv/2024/01/07/adidas2.3.jpg', 'https://img.upanh.tv/2024/01/07/adidas2.4.jpg','Primeknit upper for a lightweight and comfortable fit.',
+        'Women','Adidas', 70, 'running', 'InStock', 5.5, 'admin' );
+
+INSERT INTO `product` (`product_name`, category_id, `avartar_image_product`, `image1_product`,`image2_product`,
+                       `image3_product`,`description`,`gender`, `brand`,
+                       `price`,`sport`,`status`,`discount`,`created_by`)
+VALUES ('Adidas NMD R1 Primeknit', 2 ,'https://img.upanh.tv/2024/01/07/adidas2.1.jpg', 'https://img.upanh.tv/2024/01/07/adidas2.2.jpg',
+        'https://img.upanh.tv/2024/01/07/adidas2.3.jpg', 'https://img.upanh.tv/2024/01/07/adidas2.4.jpg','Primeknit upper for a lightweight and comfortable fit.',
+        'Unisex','Adidas', 120, 'running', 'InStock', 5.5, 'admin' );
+
+
+INSERT INTO `product` (`product_name`, category_id, `avartar_image_product`, `image1_product`,`image2_product`,
+                       `image3_product`,`description`,`gender`,`brand`,
+                       `price`,`sport`,`status`,`discount`,`created_by`)
+VALUES ('Adidas Ultra Boost 5.0', 2 ,'https://img.upanh.tv/2024/01/07/adidas1.1.jpg', 'https://img.upanh.tv/2024/01/07/adidas1.2.jpg',
+        'https://img.upanh.tv/2024/01/07/adidas1.3.jpg', 'https://img.upanh.tv/2024/01/07/adidas1.4.jpg','High-performance running shoes with Boost technology for energy return.',
+        'Men','Adidas', 270, 'running', 'InStock', 9.5, 'admin' );
+
+
+
+INSERT INTO `color` (`name`,`status`, `created_by`)
+VALUES ('clothes', 'Visible','Admin');
+
+
---
+++ Backend/ProjectSem2/target/classes/schema.sql
@@ -0,0 +1,184 @@
+CREATE TABLE IF NOT EXISTS `category` (
+   `category_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+     `name` VARCHAR(100) NOT NULL UNIQUE,
+    `status` ENUM('Visible', 'Invisible'),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `products` (
+     `product_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+     `category_id` BIGINT NOT NULL,
+     `product_name` VARCHAR(100) NOT NULL,
+    `avartar_image_product` VARCHAR(100) NOT NULL,
+    `image1_product` VARCHAR(100) NULL,
+    `image2_product` VARCHAR(100) NULL,
+    `image3_product` VARCHAR(100) NULL,
+    `description` TEXT NOT NULL,
+    `gender` VARCHAR(50) NOT NULL,
+    `brand` VARCHAR(50) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `status` ENUM('InStock', 'OutOfStock'),
+    `discount` FLOAT NULL,
+    FOREIGN KEY (`category_id`) REFERENCES category(category_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+    );
+
+
+CREATE TABLE IF NOT EXISTS warehouse (
+    `warehouse_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `product_id` BIGINT NOT NULL,
+    `size` VARCHAR(50) NOT NULL,
+    `color` VARCHAR(50) NOT NULL,
+    `quantity` INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+    );
+
+
+CREATE TABLE IF NOT EXISTS `role` (
+    `role_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `name` ENUM('ROLE_ADMIN', 'ROLE_EMPLOYEE', 'ROLE_ANONYMOUS'),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+     `user_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+     `username` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(100) NOT NULL,
+    `avatar` VARCHAR(100) NOT NULL,
+    `otp` VARCHAR(5) NULL;
+    `otp_generated_time` DATETIME;
+    `enabled` TINYINT(1) DEFAULT 0;
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+    `user_id` BIGINT NOT NULL,
+    `role_id` BIGINT NOT NULL,
+     FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    FOREIGN KEY (`role_id`) REFERENCES role(role_id),
+    PRIMARY KEY(`user_id`, `role_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `reset_password` (
+     `reset_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+     `user_id` BIGINT NOT NULL,
+     `otp_reset` VARCHAR(200) NOT NULL,
+    `reset_password_otp_expiry` DATETIME NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `address` (
+    `address_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `user_id` BIGINT NULL,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NOT NULL,
+    `country` VARCHAR(100) NOT NULL,
+    `city` VARCHAR(100) NOT NULL,
+    `address` VARCHAR(500) NOT NULL,
+    `optional` VARCHAR(200)  NULL,
+    `zipCode` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(100) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `payment` (
+    `payment_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `payment_method` ENUM('ONLINE', 'CODING'),
+    `status` ENUM('Success', 'Pending', 'Delivering', 'Cancel'),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `delivery` (
+    `delivery_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `address_id` BIGINT NOT NULL,
+    `delivery_status` ENUM('Success', 'Delivering', 'Cancel'),
+    FOREIGN KEY (`address_id`) REFERENCES address(address_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `order` (
+    `order_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `user_id` BIGINT NOT NULL,
+    `payment_id` BIGINT NOT NULL,
+    `delivery_id` BIGINT NOT NULL,
+    `total_price` DOUBLE NOT NULL,
+    `order_status` VARCHAR(100) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    FOREIGN KEY (`payment_id`) REFERENCES payment(payment_id),
+    FOREIGN KEY (`delivery_id`) REFERENCES delivery(delivery_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `carts` (
+     `cart_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `user_id` BIGINT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS `cart_item` (
+   `cart_item_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+   `product_id` BIGINT NOT NULL,
+   `cart_id` BIGINT NOT NULL,
+   `sub_total` DOUBLE NOT NULL,
+   `quantity` INT NOT NULL,
+   FOREIGN KEY (`product_id`) REFERENCES product(product_id),
+    FOREIGN KEY (`cart_id`) REFERENCES carts(cart_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `product_review` (
+    `product_review_id` BIGINT AUTO_INCREMENT  PRIMARY KEY,
+    `product_id` BIGINT NOT NULL,
+    `user_id` BIGINT NOT NULL,
+    `comment` TEXT NOT NULL,
+    `star` INT NOT NULL,
+    `status` INT NOT NULL,
+    FOREIGN KEY (`product_id`) REFERENCES product(product_id),
+    FOREIGN KEY (`user_id`) REFERENCES user(user_id),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_by` VARCHAR(50) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(50) DEFAULT NULL
+);
