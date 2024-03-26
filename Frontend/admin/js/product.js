7476ac910516cd242bdb8a5bc416a7a9577de01b
nguyenduyhoang03 <hoangtop2003@gmail.com>
2024-03-26 23:38:09 +0700

    Update logout, Add Product, edit product, Update Quantity

--- Frontend/admin/js/product.js
+++ Frontend/admin/js/product.js
@@ -1,3 +1,43 @@
+const dataTable = $('#dataTable').DataTable({
+    createdRow: function (row, data, dataIndex) {
+        $(row).find('td:last').addClass('btn-actions');
+    }
+});
+
+
+async function getProducts() {
+    try {
+        const response = await axios.get('http://localhost:8080/api/products/');
+        const data = response.data;
+        const dataTable = $('#dataTable').DataTable(); // Initialize DataTables
+        
+        // Clear existing rows in the table
+        dataTable.clear().draw();
+        
+        // Add new data rows
+        data.forEach(item => {
+            dataTable.row.add([
+                item.id,
+                `<img src="${item.img}" alt="Product Image" class="product-image">`,
+                item.name,
+                item.categoryName,
+                item.brand,
+                item.price,
+                item.discount,
+                `<td class="btn-actions">
+                    <button class="btn btn-primary" onclick="openDetailsModal('${item.id}')">Details</button>
+                    <button class="btn btn-warning" onclick="openEditProductPopup('${item.id}')">Edit</button>
+                    <button class="btn btn-warning" onclick="openUpdateModal('${item.id}')">Update</button>
+                    <button class="btn btn-danger" onclick="remove('${item._id}')">Delete</button>
+                </td>`
+            ]).draw(false);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+getProducts();
 
 // Function to open details modal and populate details
 function openDetailsModal(productId) {
@@ -27,28 +67,20 @@ function openDetailsModal(productId) {
         });
 }
 
-
-
-
-
 // Function to open edit modal and populate form fields
-function openEditModal(productId) {
+function openUpdateModal(productId) {
     axios.get(`http://localhost:8080/api/products/${productId}`)
         .then(response => {
             const product = response.data;
             document.getElementById('editProductId').value = product.id;
-            document.getElementById('editProductName').value = product.name;
-            document.getElementById('editProductCategory').value = product.categoryName;
-            document.getElementById('editProductBrand').value = product.brand;
-            document.getElementById('editProductPrice').value = product.price;
-            document.getElementById('editProductDiscount').value = product.discount;
-
+          
             if (product && product.color) {
                 const colors = product.color;
                 const sizes = product.size;
 
                 // Hiển thị size dưới dạng select
                 const sizeSelect = document.getElementById('editProductSize');
+                sizeSelect.innerHTML = '';
                 sizes.forEach(size => {
                     const option = document.createElement('option');
                     option.text = size;
@@ -58,6 +90,7 @@ function openEditModal(productId) {
 
                 // Hiển thị color dưới dạng select
                 const colorSelect = document.getElementById('editProductColor');
+                colorSelect.innerHTML = '';
                 colors.forEach(color => {
                     const option = document.createElement('option');
                     option.text = color;
@@ -84,10 +117,9 @@ function openEditModal(productId) {
                         });
                 }
 
-                // Gọi hàm cập nhật quantity ban đầu
                 updateQuantity();
 
-                const modal = new bootstrap.Modal(document.getElementById('editProductModal'));
+                const modal = new bootstrap.Modal(document.getElementById('updateProductModal'));
                 modal.show();
             } else {
                 console.error('Product colors are undefined');
@@ -101,38 +133,181 @@ function openEditModal(productId) {
 
 
 
-// Hàm cập nhật quantity
+// Update quantity or color and size
 function updateQuantityProduct(productId, color, size, quantity) {
 
     // Gửi yêu cầu API để cập nhật quantity
     axios.post(`http://localhost:8080/api/warehouse/${productId}?color=${color}&size=${size}&quantity=${quantity}`)
     .then(response => {
         console.log('Quantity updated successfully:', response.data);
-        const modal = new bootstrap.Modal(document.getElementById('editProductModal'));
+        const modal = bootstrap.Modal.getInstance(document.getElementById('updateProductModal'));
+        alert("updated successfully");
         modal.hide();
+        refreshTableData()
     })
     .catch(error => {
         console.error('Error updating quantity:', error);
     });
 }
 
-// Function to handle form submission for updating product
-document.getElementById('editProductForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission behavior
+document.getElementById('updateProductForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
 
     const productId = document.getElementById('editProductId').value;
-    const name = document.getElementById('editProductName').value; // This variable seems unused
-    const categoryName = document.getElementById('editProductCategory').value; // This variable seems unused
-    const brand = document.getElementById('editProductBrand').value; // This variable seems unused
-    const price = document.getElementById('editProductPrice').value; // This variable seems unused
-    const discount = document.getElementById('editProductDiscount').value; // This variable seems unused
-    const size = document.getElementById('editProductSize').value;
-    const color = document.getElementById('editProductColor').value;
+    let color = document.getElementById('editProductColor').value;
+    let size = document.getElementById('editProductSize').value;
     const quantity = document.getElementById('editProductQuantity').value;
 
-    // Call the updateQuantityProduct function to update quantity
+    // Lấy giá trị từ trường editProductCustomColor nếu có giá trị
+    const customColor = document.getElementById('editProductCustomColor').value.trim();
+    if (customColor) {
+        color = customColor;
+    }
+
+    // Lấy giá trị từ trường editProductCustomSize nếu có giá trị
+    const customSize = document.getElementById('editProductCustomSize').value.trim();
+    if (customSize) {
+        size = customSize; // Sử dụng giá trị customSize nếu có
+    }
+
+    // Gọi hàm cập nhật quantity
     updateQuantityProduct(productId, color, size, quantity);
 
-
+    //reset form
+    document.getElementById('updateProductForm').reset();
 });
 
+
+// Function to open popup
+function openPopup() {
+    var modal = new bootstrap.Modal(document.getElementById('addProductModal'));
+    modal.show();
+}
+
+// Function to add product
+function addProduct(event) {
+    event.preventDefault();
+
+    // Retrieve form data
+    var name = document.getElementById("productName").value;
+    var img = document.getElementById("productImg").value;
+    var img1 = document.getElementById("productImg1").value;
+    var img2 = document.getElementById("productImg2").value;
+    var img3 = document.getElementById("productImg3").value;
+    var description = document.getElementById("productDescription").value;
+    var gender = document.getElementById("productGender").value;
+    var brand = document.getElementById("productBrand").value;
+    var price = parseFloat(document.getElementById("productPrice").value);
+    var discount = parseFloat(document.getElementById("productDiscount").value);
+    var categoryName = document.getElementById("productCategory").value;
+    var size = document.getElementById("productSize").value.split(",").map(item => item.trim());
+    var color = document.getElementById("productColor").value.split(",").map(item => item.trim());
+
+    var productData = {
+        "name": name,
+        "img": img,
+        "img1": img1,
+        "img2": img2,
+        "img3": img3,
+        "description": description,
+        "gender": gender,
+        "brand": brand,
+        "price": price,
+        "discount": discount,
+        "categoryName": categoryName,
+        "size": size,
+        "color": color
+    };
+
+    // Send POST request to add product
+    axios.post("http://localhost:8080/api/products/addProduct", productData)
+        .then(function(response) {
+            console.log(response.data);
+            alert("Product added successfully!");
+            var modal = new bootstrap.Modal(document.getElementById('addProductModal'));
+            modal.hide();
+        })
+        .catch(function(error) {
+            console.error(error);
+            alert("Failed to add product. Please try again later.");
+        });
+}
+
+document.getElementById("addProductForm").addEventListener("submit", addProduct);
+
+
+// Function to open edit product popup and populate form fields
+function openEditProductPopup(productId) {
+    axios.get(`http://localhost:8080/api/products/${productId}`)
+        .then(response => {
+            const product = response.data;
+
+            document.getElementById('productId').value = product.id;
+            document.getElementById('editProductName').value = product.name;
+            document.getElementById('editProductImg').value = product.avatar;
+            document.getElementById('editProductImg1').value = product.img1;
+            document.getElementById('editProductImg2').value = product.img2;
+            document.getElementById('editProductImg3').value = product.img3;
+            document.getElementById('editProductDescription').value = product.desc;
+            document.getElementById('editProductGender').value = product.gender;
+            document.getElementById('editProductBrand').value = product.brand;
+            document.getElementById('editProductPrice').value = product.price;
+            document.getElementById('editProductDiscount').value = product.discount;
+            document.getElementById('editProductCategoryName').value = product.categoryName;
+
+            // Show the edit product modal
+            const modal = new bootstrap.Modal(document.getElementById('editProductModal'));
+            modal.show();
+        })
+        .catch(error => {
+            console.error('Error fetching product data:', error);
+        });
+}
+
+// Function to update product
+function editProduct(event) {
+    event.preventDefault();
+
+    // Retrieve form data
+    const productId = document.getElementById('productId').value;
+    const name = document.getElementById('editProductName').value;
+    const img = document.getElementById('editProductImg').value;
+    const img1 = document.getElementById('editProductImg1').value;
+    const img2 = document.getElementById('editProductImg2').value;
+    const img3 = document.getElementById('editProductImg3').value;
+    const description = document.getElementById('editProductDescription').value;
+    const gender = document.getElementById('editProductGender').value;
+    const brand = document.getElementById('editProductBrand').value;
+    const price = parseFloat(document.getElementById('editProductPrice').value);
+    const discount = parseFloat(document.getElementById('editProductDiscount').value);
+    const categoryName = document.getElementById('editProductCategoryName').value;
+
+    // Prepare product data
+    const productData = {
+        name: name,
+        img: img,
+        img1: img1,
+        img2: img2,
+        img3: img3,
+        description: description,
+        gender: gender,
+        brand: brand,
+        price: price,
+        discount: discount,
+        categoryName: categoryName
+    };
+
+    axios.patch(`http://localhost:8080/api/products/${productId}`, productData)
+        .then(response => {
+            console.log('Product updated successfully:', response.data);
+            alert("Product updated successfully!");
+            $('#editProductModal').modal('hide');
+            getProducts();
+        })
+        .catch(error => {
+            console.error('Error updating product:', error);
+            alert("Failed to update product. Please try again later.");
+        });
+}
+
+document.getElementById("editProductForm").addEventListener("submit", editProduct);
